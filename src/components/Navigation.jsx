b080584363d2f617708dadac8117ace8960dd536
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2023-03-07 23:30:48 +0100

    added theme toggle button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
+// import ThemeToggle from './ThemeToggle.jsx';
 
-import "../css/navigation.css";
+import '../css/navigation.css';
 
 export default function Navigation() {
   const [navb, setNavb] = useState(true);
   const handleClick = () => setNavb(!navb);
   const [click, setClick] = useState(true);
   const handleMenu = () => setNavb(!navb);
-  const footer = "#footer";
+  const footer = '#footer';
   return (
     <header className="header">
       <div className="nav-container">
@@ -24,8 +25,8 @@ export default function Navigation() {
               height="30"
               viewBox="0 0 24 24"
             >
-              {" "}
-              <title>GitHub</title>{" "}
+              {' '}
+              <title>GitHub</title>{' '}
               <path
                 d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z"
                 fill="#fff"
@@ -72,6 +73,7 @@ export default function Navigation() {
               </g>
             </svg>
           </a>
+          {/* <ThemeToggle /> */}
         </div>
         <nav className="desktop-nav nav">
           <ul className="desktop-navItems">
@@ -119,7 +121,7 @@ export default function Navigation() {
         </nav>
 
         <button
-          className="btn"
+          className="nav-toggler"
           role="navigation"
           aria-label="menu"
           onClick={handleClick}
@@ -139,9 +141,9 @@ export default function Navigation() {
       </div>
 
       <nav className="mobile-nav nav">
-        <ul className={!navb || !click ? "display-mobile-nav" : "display-none"}>
+        <ul className={!navb || !click ? 'display-mobile-nav' : 'display-none'}>
           <li>
-            {" "}
+            {' '}
             <a
               className="mobile-navLink"
               onClick={handleMenu}
@@ -152,7 +154,7 @@ export default function Navigation() {
           </li>
 
           <li>
-            {" "}
+            {' '}
             <a
               className="mobile-navLink"
               onClick={handleMenu}
